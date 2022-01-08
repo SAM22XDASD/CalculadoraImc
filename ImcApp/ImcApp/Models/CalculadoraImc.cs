2198b73a7e90bcd88209d1e9c8b2f66d5fcebd43
Samir Gonzalo Aes Lopez <17470067@campeche.tecnm.mx>
2022-01-08 12:55:19 -0600

    Commit 08/01/2022

---
+++ ImcApp/ImcApp/Models/CalculadoraImc.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImcApp.Models
+{
+    public class CalculadoraImc
+    {
+
+        //public decimal Peso { get; set; }
+        //public decimal Estatura { get; set; }
+
+        //public CalculadoraImc(decimal peso, decimal estatura)
+        //{
+        //    Peso = peso;
+        //    Estatura = estatura;
+        //}
+
+        //public decimal Imc
+        //{
+        //    get
+        //    {
+        //        return Peso / (Estatura * Estatura);
+        //    }
+        //}
+
+        //public enum EstadoNutricional
+        //{
+        //    PesoBajo,
+        //    PesoNormal,
+        //    SobrePeso,
+        //    Obesidad,
+        //    ObesidadExtrema
+        //}
+
+        //public EstadoNutricional SituacionNutricional
+        //{
+        //    get
+        //    {
+        //        decimal imc = Imc;
+        //        if (imc < 18.5M)
+        //        {
+        //            return EstadoNutricional.PesoBajo;
+        //        }
+        //        else if (imc < 25.0M)
+        //        {
+        //            return EstadoNutricional.PesoNormal;
+        //        }
+        //        else if (imc < 30.0M)
+        //        {
+        //            return EstadoNutricional.SobrePeso;
+        //        }
+        //        else if (imc < 40.0M)
+        //        {
+        //            return EstadoNutricional.Obesidad;
+        //        }
+        //        else
+        //        {
+        //            return EstadoNutricional.ObesidadExtrema;
+        //        }
+
+        //    }
+        //}
+
+        public static decimal IndiceDeMasaCorporal(decimal peso, decimal estatura) => peso / (estatura * estatura);
+
+
+        public enum EstadoNutricional
+        {
+            PesoBajo,
+            PesoNormal,
+            SobrePeso,
+            Obesidad,
+            ObesidadExtrema
+        }
+
+        public static EstadoNutricional SituacionNutricional(decimal imc)
+        {
+            if (imc < 18.5M)
+            {
+                return EstadoNutricional.PesoBajo;
+            }
+            else if (imc < 25.0M)
+            {
+                return EstadoNutricional.PesoNormal;
+            }
+            else if (imc < 30.0M)
+            {
+                return EstadoNutricional.SobrePeso;
+            }
+            else if (imc < 40.0M)
+            {
+                return EstadoNutricional.Obesidad;
+            }
+            else
+            {
+                return EstadoNutricional.ObesidadExtrema;
+            }
+        }     
+    }
+}
---
+++ ImcApp/ImcApp/Views/MainPage.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using ImcApp.Models;
+using ImcApp.ViewModels;
+
+namespace ImcApp.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        private MainPageViewModel viewModel;
+        public MainPage()
+        {
+            InitializeComponent();
+            viewModel = new MainPageViewModel();
+            BindingContext = viewModel;
+            //LimpiarIU();
+        }
+
+        private void calcularButton_Clicked(object sender, EventArgs e)
+        {
+            //decimal peso;
+            //decimal estatura;
+            //bool pesoEsConvertible = decimal.TryParse(pesoEntry.Text, out peso);
+            //bool estaturaEsConvertible = decimal.TryParse(estaturaEntry.Text, out estatura);
+            //if (pesoEsConvertible && estaturaEsConvertible)
+            //{
+            //    CalculadoraImc cimc = new CalculadoraImc(peso, estatura);
+            //    imcLabel.Text = string.Format("{0:F4}", cimc.Imc);
+            //    situacionNutricionalLabel.Text = GetEstadoNutricional(cimc.SituacionNutricional);
+            //}
+
+            viewModel.Imc = CalculadoraImc.IndiceDeMasaCorporal(viewModel.Peso, viewModel.Estatura);
+            viewModel.SituacionNutricional = GetEstadoNutricional(CalculadoraImc.SituacionNutricional(viewModel.Imc));
+        }
+        private void limpiarButton_Clicked(object sender, EventArgs e)
+        {
+            LimpiarIU();
+        }
+
+        private void LimpiarIU()
+        {
+            //pesoEntry.Text = string.Empty;
+            //estaturaEntry.Text = string.Empty;
+            //imcLabel.Text = string.Empty;
+            //situacionNutricionalLabel.Text = string.Empty;
+        }
+
+
+        private string GetEstadoNutricional(CalculadoraImc.EstadoNutricional estatus)
+        {
+
+            switch (estatus)
+            {
+                case CalculadoraImc.EstadoNutricional.PesoBajo:
+                    return Resources["TextoPesoBajo"] as string;
+                case CalculadoraImc.EstadoNutricional.PesoNormal:
+                    return Resources["TextoPesoNormal"] as string;
+                case CalculadoraImc.EstadoNutricional.SobrePeso:
+                    return Resources["TextoSobrePeso"] as string;
+                case CalculadoraImc.EstadoNutricional.Obesidad:
+                    return Resources["TextoObesidad"] as string;
+                case CalculadoraImc.EstadoNutricional.ObesidadExtrema:
+                    return Resources["TextoObesidadExtrema"] as string;
+                default:
+                    return string.Empty;
+            }
+
+        }
+    }
+}
